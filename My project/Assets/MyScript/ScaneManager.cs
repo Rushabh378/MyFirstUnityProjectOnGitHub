b597b5714962122b9612a270c4ddf633b96dffa4
Rushabh Gohel <103501258+Rushabh378@users.noreply.github.com>
2022-04-17 23:27:48 +0530

    Challange Complete
    added menu and a back button

--- My project/Assets/MyScript/ScaneManager.cs
+++ My project/Assets/MyScript/ScaneManager.cs
@@ -6,10 +6,21 @@ using UnityEngine.SceneManagement;
 public class ScaneManager : MonoBehaviour
 {
     public Button button;
+    // when for next button is clicked
     void onButtonClick()
     {
         Debug.Log("button has been clicked.");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    //go to main scene
+    public void toMain()
+    {
+        SceneManager.LoadScene(0);
+    }
+    // to go back
+    public void toBack()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
     // Start is called before the first frame update
     void Start()
