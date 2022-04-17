2370cce9eb2121e1ad6cd50a71151d129945a9b6
Rushabh Gohel <103501258+Rushabh378@users.noreply.github.com>
2022-04-17 16:31:20 +0530

    created scene management system

---
+++ My project/Assets/MyScript/ScaneManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class ScaneManager : MonoBehaviour
+{
+    public Button button;
+    void onButtonClick()
+    {
+        Debug.Log("button has been clicked.");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        button.onClick.AddListener(onButtonClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
